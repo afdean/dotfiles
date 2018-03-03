a6b87ac0ac22cffd190341a069c0da8680039262
Andrew Dean <afdean00@gmail.com>
2018-03-03 20:30:54 +0100

    Make a git setup script for settings

---
+++ setup/git.sh
@@ -0,0 +1,6 @@
+# Author: Andrew Dean
+# Description: Commands to have git setup once installed
+
+git config --global core.excludesfile ~/.gitignore
+git config --global user.name "Andrew Dean"
+git config --global core.editor vim
