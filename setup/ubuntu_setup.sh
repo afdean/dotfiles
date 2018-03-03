a73c7e9a7eab18002720d4b771b36afc876f1e1e
AFDean00 <afdean00@gmail.com>
2018-03-03 19:49:01 +0100

    Update ubuntu setup

--- setup/ubuntu_setup.sh
+++ setup/ubuntu_setup.sh
@@ -21,8 +21,14 @@ apt -y install snapd
 #
 
 ## Third Party
+# Android Studio
+add-apt-repository -y ppa:maarten-fonville/android-studio
 # Atom
 add-apt-repository -y ppa:webupd8team/atom
+# Lutris
+ver=$(lsb_release -sr); if [ $ver != "17.10" -a $ver != "17.04" -a $ver != "16.04" ]; then ver=16.04; fi 
+echo "deb http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
+wget -q http://download.opensuse.org/repositories/home:/strycore/xUbuntu_$ver/Release.key -O- | sudo apt-key add -
 # Mesa
 add-apt-repository -y ppa:ubuntu-x-swat/updates
 # Timeshift
@@ -48,28 +54,33 @@ apt update
 #
 
 # Using default openjdk for java
+apt -y install ant
+apt -y install git
+apt -y install maven
 apt -y install python
 apt -y install python3
 apt -y install pip
-apt -y install ant
-apt -y install maven
+apt -y install r-base r-base-dev
 pip install certifi
-pip install scipy
+pip install django
 pip install matplotlib
 pip install numpy
 pip install pandas
 pip install pylint
 pip install scikit-learn
+pip install scipy
 pip install uncompyle2
 pip install virtualenv
-pip install django
 
 # ----------------------------------------------------------------------------
 # Software
 #
 
+apt -y install android-studio
 apt -y install anki
+apt -y install ardour
 apt -y install atom
+apt -y install audacity
 apt -y install bleachbit
 apt -y install blender
 apt -y install brave
@@ -83,15 +94,13 @@ apt -y install dolphin-emu
 apt -y install nautilus-dropbox
 apt -y install eclipse
 apt -y install gnome-tweak-tool
-apt -y install libffado2
 apt -y install filezilla
 apt -y install firefox
-apt -y install git
 apt -y install handbrake
-apt -y install jackd
 snap install keepassxc
 apt -y install krita
 apt -y install libreoffice
+apt -y install lutris
 apt -y install lxrandr
 apt -y install musescore
 apt -y install octave
@@ -107,12 +116,47 @@ apt -y install synaptic
 apt -y install timeshift
 apt -y install thunderbird
 apt -y install torbrowser-launcher
+apt -y install vagrant
 apt -y install veracrypt
 apt -y install virtualbox
 apt -y install vlc
 apt -y install wine
 
 # ----------------------------------------------------------------------------
+# Ubuntu Studio
+# Sets Ubuntu up to handle audio work
+# Info pulled from https://help.ubuntu.com/community/UbuntuStudioPreparation:w
+# Be aware, info might be outdated
+
+apt -y install jackd
+apt -y install libffado2
+apt -y install linux-lowlatency
+apt -y install qjackctl
+
+## Firewire specific setup
+adduser $USER audio
+apt -y install ffado-mixer-qt4 
+apt -y install jackd2-firewire
+apt -y install pulseaudio-module-jack 
+apt -y install patchage
+
+# ----------------------------------------------------------------------------
+# Restricted Extras
+# Based upon 16.04(xenial) package except without flash
+# https://packages.ubuntu.com/en/xenial/ubuntu-restricted-extras
+# Will need to be updated for future versions
+
+apt -y install libavcodec-extra
+apt -y install gstreamer1.0-fluendo-mp3
+apt -y install gstreamer1.0-libav
+apt -y install gstreamer1.0-plugins-bad
+apy -y install gstreamer1.0-plugins-ugly
+apt -y install oxideqt-codecs-extra 
+apy -y install ttf-mscorefonts-installer
+apy -y insall unrar
+
+# ----------------------------------------------------------------------------
+
 # Final
 
 # Set wallpaper
@@ -129,6 +173,9 @@ reboot
 # Unfinished
 
 # Enable Ubuntu universe ppa
+# ?
+
+# Fixubuntu recommendations
 # ?
 
 # Use LXrandr to set up monitors correctly
